1b35499efb9f835869e0cd4c64493f02116c7b5d
gnishida <gnishida@purdue.edu>
2014-09-06 00:29:27 -0400

    Initial commit.

---
+++ data/Solution.py
@@ -0,0 +1,28 @@
+def DecisionTree():
+	#TODO: Your code starts from here. 
+        #      This function should return a list of labels.
+	#      e.g.: 
+	#	labels = [['+','-','+'],['+','+','-'],['-','+'],['+','+']]
+       	#	return labels
+	#	where:
+	#		labels[0] = original_training_labels
+	#		labels[1] = prediected_training_labels
+	#		labels[2] = original_testing_labels
+	#		labels[3] = predicted_testing_labels
+
+	return
+
+
+def DecisionTree(maxDepth):
+	#TODO: Your code starts from here.
+    #      This function should return a list of labels.
+	#      e.g.:
+	#	labels = [['+','-','+'],['+','+','-'],['-','+'],['+','+']]
+    #	return labels
+	#	where:
+	#		labels[0] = original_training_labels
+	#		labels[1] = prediected_training_labels
+	#		labels[2] = original_testing_labels
+	#		labels[3] = predicted_testing_labels
+    
+	return
